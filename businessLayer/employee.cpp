93cf72b538352742187fe42e8af5e0f01b73d13a
Scarlet Salinas <2022156127@ub.edu.bz>
2026-01-26 01:33:43 -0600

    implemented Employee class member functions

--- businessLayer/employee.cpp
+++ businessLayer/employee.cpp
@@ -4,3 +4,68 @@
 #include "Employee.h"
 #include <iostream>
 using namespace std;
+
+//Constructor
+Employee::Employee(int emp_id, string first_name, string last_name, int dept_id) {
+    id = emp_id;
+    fname = first_name;
+    lname = last_name;
+    dept = dept_id;
+}
+
+// Default constructor
+Employee::Employee() {
+    id = 0;
+    fname = "";
+    lname = "";
+    dept = 0;
+}
+
+//set functions
+void Employee::setId(int emp_id) {
+    id = emp_id;
+}
+
+void Employee::setFname(string first_name) {
+    fname = first_name;
+}
+
+void Employee::setLname(string last_name) {
+    lname = last_name;
+}
+
+void Employee::setDept(int dept_id) {
+    dept = dept_id;
+}
+
+void Employee::setEmployee(int emp_id, string first_name, string last_name, int dept_id) {
+    id = emp_id;
+    fname = first_name;
+    lname = last_name;
+    dept = dept_id;
+}
+
+//get functions
+int Employee::getId() {
+    return id;
+}
+
+string Employee::getFname() {
+    return fname;
+}
+
+string Employee::getLname() {
+    return lname;
+}
+
+int Employee::getDept() {
+    return dept;
+}
+
+// Display function
+void Employee::displayEmployee() {
+    cout << "Employee ID: " << id << endl;
+    cout << "Name: " << fname << " " << lname << endl;
+    cout << "Department ID: " << dept << endl;
+    cout << "---------------------" << endl;
+}
